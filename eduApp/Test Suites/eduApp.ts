34059642f619b49ab0be9f031bb4c87100b6ee9d
Faiz Oktarivandi <faizoktarivandi@gmail.com>
2024-01-07 15:29:40 +0700

    reupload Katalon repository

---
+++ eduApp/Test Suites/eduApp.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Test Case selama pembelajaran materi Eduwork</description>
+   <name>eduApp</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>613dc2c6-79ff-48c8-97c3-266ba37d8773</testSuiteGuid>
+   <testCaseLink>
+      <guid>73cc2149-ad3c-48d1-a458-a21e11c5eeaa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/appPendaki/detailPeralatan_appPendaki</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>094ce812-fa40-4038-82e8-a9f6d8e70ca8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/samsungShop/samsungShopFix</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9a49eeab-aa4a-45af-a131-7af40298f0b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Calculator/kalkulatorBasic</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
